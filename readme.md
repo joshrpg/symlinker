4a70f60084bf7dc63d32c1d774da015233aac9ee
joshrpg <xjoshrpgx@gmail.com>
2020-11-18 21:45:09 -0600

    init commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Symlinker.exe
+
+## How to build
+
+### Prerec
+
+* [VS Code](https://code.visualstudio.com/Download)
+* [C# Extension](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp)
+* [.NET Core](https://dotnet.microsoft.com/download)
+
+### Build
+
+1. Clone the repo
+2. Use cmd to navigate to the src folder in the repo folder.
+3. Run the following `dotnet run`.
+
+```Batch
+git clone https://github.com/joshrpg/symlinker.git
+cd symlinker\src
+dotnet run
+```
---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace link
+{
+    public class symlink
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+      
+        static void Main(string[] args)
+        {
+            string link = @"C:\Users\joshu\GitHub\mine\symlinker\.vscode\a.txt";
+            string target = @"C:\Users\joshu\GitHub\mine\symlinker\.vscode\b.txt";
+
+            if (Directory.Exists(link) || File.Exists(link)) {
+                CreateSymbolicLink(target, link, 0);
+
+                if (Directory.Exists(target) || File.Exists(target)) {
+                    Console.WriteLine("SYMLINK created for {0} <------> {1}", link, target);
+                } else {
+                    Console.WriteLine("Symlink failed to be created.");
+                }
+            } else {
+                Console.WriteLine("{0} does not exist.", link);
+            }
+        }
+    }
+}
